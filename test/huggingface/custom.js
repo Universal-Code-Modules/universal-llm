bddc8d7f0fe58904e0064debb49fc340732b8e4a
Dmitriy Tselinko <dmitriy.tselinko@extremereach.com>
2024-05-15 10:54:47 +0200

    Huggingface refactor PR-URL: https://github.com/Universal-Code-Modules/universal-llm/pull/3

---
+++ test/huggingface/custom.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { beforeEach, it, describe } = require('node:test');
+const assert = require('node:assert');
+
+const { huggingface } = require('../../lib');
+const utils = require('../../lib/huggingface/utils');
+
+const { Chat } = huggingface;
+
+const API_KEY = process.env.HUGGINGFACE_API_KEY;
+
+const {
+  custom: uCustom,
+} = utils;
+
+describe('custom', () => {
+  let custom;
+
+  beforeEach(() => {
+    const chat = new Chat({ apiKey: API_KEY });
+    custom = uCustom(chat.hf);
+  });
+
+  // TODO: fix test, response is undefined for some reason
+  it.skip('customCall', async () => {
+    const res = await custom.customCall('hello world');
+
+    console.log(res);
+  });
+
+  it('customCallStreaming', async () => {
+    const res = await custom.customCallStreaming('hello world');
+
+    assert.ok(typeof res === 'object');
+  });
+
+  // TODO: To test this one we need to have own inference endpoint
+  it.skip('customInferenceEndpoint', async () => {
+    const endpoint =
+      'https://xyz.eu-west-1.aws.endpoints.huggingface.cloud/gpt2';
+    const res = await custom.customInferenceEndpoint(
+      'The answer to the universe is',
+      endpoint,
+    );
+
+    console.log(res);
+  });
+});
---
+++ test/huggingface/multimodal.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const { beforeEach, it, describe } = require('node:test');
+const assert = require('node:assert');
+const path = require('node:path');
+const { readFileSync } = require('node:fs');
+
+const { huggingface } = require('../../lib');
+const utils = require('../../lib/huggingface/utils');
+
+const { Chat } = huggingface;
+
+const API_KEY = process.env.HUGGINGFACE_API_KEY;
+
+const {
+  multimodal: uMultimodal,
+} = utils;
+
+const FILES = process.cwd() + '/files/huggingface/';
+const IMAGES = FILES + 'images';
+
+const testCatFile = readFileSync(path.join(IMAGES, 'cat.jpg'));
+const testInvoiceFile = readFileSync(path.join(IMAGES, 'invoice.png'));
+
+describe('multimodal', () => {
+  let multimodal;
+
+  beforeEach(() => {
+    const chat = new Chat({ apiKey: API_KEY });
+    multimodal = uMultimodal(chat.hf);
+  });
+
+  it('featureExtraction', async () => {
+    const res = await multimodal.featureExtraction('That is a happy person');
+
+    assert.ok(Array.isArray(res));
+    assert.ok(res.every((el) => typeof el === 'number'));
+  });
+
+  it('visualQuestionAnswering', async () => {
+    const inputs = {
+      question: 'How many cats are lying down?',
+      image: new Blob([testCatFile]),
+    };
+    const res = await multimodal.visualQuestionAnswering(inputs);
+
+    assert.ok(typeof res === 'object');
+    assert.ok('score' in res);
+    assert.ok('answer' in res);
+    assert.ok(typeof res.score === 'number');
+    assert.strictEqual(res.answer, '1');
+  });
+
+  it('documentQuestionAnswering', async () => {
+    const inputs = {
+      question: 'Invoice number?',
+      image: new Blob([testInvoiceFile]),
+    };
+    const res = await multimodal.documentQuestionAnswering(inputs);
+
+    assert.ok(typeof res === 'object');
+    assert.ok('score' in res && typeof res.score === 'number');
+    assert.ok('start' in res && typeof res.start === 'number');
+    assert.ok('end' in res && typeof res.end === 'number');
+    assert.ok('answer' in res && typeof res.answer === 'string');
+    assert.strictEqual(res.answer, 'us-001');
+  });
+});
